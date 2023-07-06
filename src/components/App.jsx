c089f5a091aa3df73e00f2e8333f878e0b48fca7
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-07-05 21:59:36 -0500

    a

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,11 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
+      {/* pruebas commit */}
     </div>
   );
 };
